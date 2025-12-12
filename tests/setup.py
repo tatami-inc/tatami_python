bfbe9abdd049ee70c6b31fc5ac7b1947e6917dde
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-12-12 17:55:15 +1100

    Added test and install checks, removed requirement for assorthead.
    Also added cmake/ and extern/ directories for proper builds.

--- tests/setup.py
+++ tests/setup.py
@@ -29,14 +29,12 @@ class build_ext(build_ext_orig):
         outpath = os.path.join(build_lib.absolute(), ext.name) 
 
         if not os.path.exists(build_temp):
-            import assorthead
             cmd = [ 
                 "cmake", 
                 "-S", "lib",
                 "-B", build_temp,
                 "-Dpybind11_DIR=" + os.path.join(os.path.dirname(pybind11.__file__), "share", "cmake", "pybind11"),
-                "-DPYTHON_EXECUTABLE=" + sys.executable,
-                "-DASSORTHEAD_INCLUDE_DIR=" + assorthead.includes()
+                "-DPYTHON_EXECUTABLE=" + sys.executable
             ]
             if os.name != "nt":
                 cmd.append("-DCMAKE_BUILD_TYPE=Release")
