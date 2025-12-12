e1ecdb849f170b6705384b1a13e87ccfbba65355
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-12-12 13:51:33 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_python@e2404063d23cb6427bca5d9f7941bc7eac1c4495 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -147,6 +147,7 @@ var searchData=
   ['densematrix_2ehpp_144',['DenseMatrix.hpp',['https://tatami-inc.github.io/tatami/DenseMatrix_8hpp.html',1,'']]],
   ['densematrix_3c_20value_5f_2c_20index_5f_2c_20storage_5f_20_3e_145',['DenseMatrix&lt; Value_, Index_, Storage_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1DenseMatrix.html',1,'tatami']]],
   ['denserowmatrix_146',['DenseRowMatrix',['https://tatami-inc.github.io/tatami/classtatami_1_1DenseRowMatrix.html',1,'tatami::DenseRowMatrix&lt; typename Value_, typename Index_, class Storage_ &gt;'],['https://tatami-inc.github.io/tatami/classtatami_1_1DenseRowMatrix.html#a17956d69228777bc58124f80ea1eb37e',1,'tatami::DenseRowMatrix::DenseRowMatrix()']]],
-  ['difference_5ftype_147',['difference_type',['https://tatami-inc.github.io/tatami/structtatami_1_1SomeNumericArray_1_1Iterator.html#aefb9a8cadab1f35ebe83a01e76c2520f',1,'tatami::SomeNumericArray::Iterator']]],
-  ['dimensionselectiontype_148',['DimensionSelectionType',['https://tatami-inc.github.io/tatami/namespacetatami.html#a0a2ecaf58e2b69bb4a808e814aeb16a1',1,'tatami']]]
+  ['deployment_147',['Deployment',['../index.html#autotoc_md4',1,'']]],
+  ['difference_5ftype_148',['difference_type',['https://tatami-inc.github.io/tatami/structtatami_1_1SomeNumericArray_1_1Iterator.html#aefb9a8cadab1f35ebe83a01e76c2520f',1,'tatami::SomeNumericArray::Iterator']]],
+  ['dimensionselectiontype_149',['DimensionSelectionType',['https://tatami-inc.github.io/tatami/namespacetatami.html#a0a2ecaf58e2b69bb4a808e814aeb16a1',1,'tatami']]]
 ];
--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -23,5 +23,6 @@ var searchData=
   ['oraculardenseextractor_20',['OracularDenseExtractor',['https://tatami-inc.github.io/tatami/classtatami_1_1OracularDenseExtractor.html',1,'tatami']]],
   ['oraculardenseextractor_3c_20value_5f_2c_20index_5f_20_3e_21',['OracularDenseExtractor&lt; Value_, Index_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1OracularDenseExtractor.html',1,'tatami']]],
   ['oracularsparseextractor_22',['OracularSparseExtractor',['https://tatami-inc.github.io/tatami/classtatami_1_1OracularSparseExtractor.html',1,'tatami']]],
-  ['oracularsparseextractor_3c_20value_5f_2c_20index_5f_20_3e_23',['OracularSparseExtractor&lt; Value_, Index_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1OracularSparseExtractor.html',1,'tatami']]]
+  ['oracularsparseextractor_3c_20value_5f_2c_20index_5f_20_3e_23',['OracularSparseExtractor&lt; Value_, Index_ &gt;',['https://tatami-inc.github.io/tatami/classtatami_1_1OracularSparseExtractor.html',1,'tatami']]],
+  ['overview_24',['Overview',['../index.html#autotoc_md1',1,'']]]
 ];
